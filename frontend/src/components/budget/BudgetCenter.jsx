f4a8d0fdec9c750caade5c53914138459b47885c
limjiehao <jiehao15@gmail.com>
2022-07-14 00:54:45 +0800

    31st commit - removed budget header and body component

--- frontend/src/components/budget/BudgetCenter.jsx
+++ frontend/src/components/budget/BudgetCenter.jsx
@@ -1,22 +1,54 @@
-import BudgetHeader from "./BudgetHeader";
-import BudgetBody from "./BudgetBody";
+const BudgetCenter = ({ month, budget, handleChangeBudget, populateDataBudget}) => {
+  //calculate the min month and max month
+  let minMonth = parseInt(month.substring(0, 4)) - 2 + "-01";
+  let maxMonth = parseInt(month.substring(0, 4)) + 2 + "-12";
 
-const BudgetCenter = ({ month, budget, handleChangeBudget, populateDataBudget }) => {
+  // Convert Month into words
+  const monthWord = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",];
+  const currentMonth = month.substring(month.length - 1, month.length);
+  const year = month.substring(0, 4);
 
-  const handleChangeBC = (str) => {
-    handleChangeBudget(str)
+  const handleChangeBC = (event) => {
+    handleChangeBudget(event.target.value);
   };
 
   const populateDataBC = () => {
-    populateDataBudget()
+    populateDataBudget();
   };
 
   return (
     <div className="budgetcenter">
-      <BudgetHeader month={month} budget={budget} handleChangeBC={handleChangeBC} populateDataBC={populateDataBC}/>
+      <label htmlFor="start">Start month:</label>
+      <input
+        type="month"
+        id="start"
+        name="start"
+        min={minMonth}
+        max={maxMonth}
+        value={month}
+        onChange={() => handleChangeBC(event)}
+      />
+      {budget.length === 0 ? (<button onClick={() => populateDataBC()}>Start planning</button>) : null}
       <br />
       <br />
-      <BudgetBody month={month} budget={budget}/>
+      <div className="budgettitle">
+        <div>Budget for {monthWord[currentMonth - 1] + " " + year}</div>
+        {/* <button onClick={() => populateDataBC()}>Add Item</button> */}
+      </div>
+      <div className="budgettable">
+        <div className="budgetitem">Type</div>
+        <div className="budgetitem">Category</div>
+        <div className="budgetitem">Planned</div>
+        <div className="budgetitem">Actual</div>
+      </div>
+      {budget.map((data) => (
+        <div className="budgettable" key={data.id}>
+          <div className="budgetitem">{data.type}</div>
+          <div className="budgetitem">{data.name}</div>
+          <div className="budgetitem">{data.planned_amt.toFixed(2)}</div>
+          <div className="budgetitem">to calculate</div>
+        </div>
+      ))}
     </div>
   );
 };
